b2f03f2b9a9344adc4e8c9347c844fa80d5030b2
Lindsay Trenton <lt353@hawaii.edu>
2025-04-13 12:44:16 -1000

    Whoops, no this is the starting point

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,22 +2,8 @@ import React from "react";
 
 var isDone = false;
 
-function strike() {
-  isDone = !isDone;
-}
-
-function unStrike() {
-  isDone = false;
-}
-
 function App() {
-  return (
-    <div>
-      <p style={isDone ? { textDecoration: "line-through" } : null}>Buy milk</p>
-      <button onClick={strike}>Change to strike through</button>
-      <button onClick={unStrike}>Change back</button>
-    </div>
-  );
+  return <p>Buy milk</p>;
 }
 
 export default App;
